9cb96416ae70cb30e7ba71e2a603361fad9c89b6
mohamed <mohamed>
2025-04-07 13:10:52 +0200

    second update

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-// Get all list items inside the shuffle list
-const shuffleList = document.querySelectorAll('.shuffle li');
+const shuffle = document.querySelectorAll(".shuffle li")
+const shuffle_arry = Array.from(shuffle);
 
+const images = document.querySelectorAll(".imgs-container .box")
+const images_array = Array.from(images)
 
-shuffleList.forEach(item => {
-    item.addEventListener("click", function () {
-        shuffleList.forEach(li => li.classList.remove("active"));
-        this.classList.add("active");
-        // this.classList.add('active');
+shuffle_arry.forEach(item => {
+    item.addEventListener("click", function (e) {
+        shuffle_arry.forEach(li => li.classList.remove("active"))
+        e.currentTarget.classList.add("active")
+        images_array.forEach(image => {
+            image.style.display = "none";
+            // document.querySelector(e.currentTarget.dataset.content).style.display = "block";
+        })
+        const targetImages = document.querySelectorAll(e.currentTarget.dataset.content);
+        targetImages.forEach(image => {
+            image.style.display = "block";
+        });
     })
-})
+});
